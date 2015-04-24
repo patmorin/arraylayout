1967a5d5f33f73eba43b8c8b4766e20d81414a66
Pat Morin <morin@scs.carleton.ca>
2015-04-23 22:50:31 -0400

    Got the iterative version working

--- veb.py
+++ veb.py
@@ -79,8 +79,45 @@ def build_binary_tree(h):
         return None
     return Node(build_binary_tree(h-1), build_binary_tree(h-1))
 
+def sequencer(h, s, d):
+    if h == 0: return
+    h0 = h//2
+    h1 = h-h0-1 
+    sequencer(h0, s, d)
+    # (add, multiply, base depth, number of bits of p)
+    s[d+h0] = (2**(h0+1)-1, 2**(h1+1)-1, d, h0+1)
+    sequencer(h1, s, d+h0+1)
+
+def try_it2(a, h, x):
+    # The magic sequence
+    s = [None]*(h+1)
+    sequencer(h, s, 0)
+    s[h] = (0, 0, h, 0)
+
+    rtl = [None]*(h+1)   # the root to leaf path
+
+    y = -1
+    i = 0
+    p = 0
+    for d in range(h+1): 
+        rtl[d] = i
+        if x < a[i][0]:
+            print "{} < {}".format(x, a[i][0])
+            p = p*2
+            y = a[i][0]
+        elif x > a[i][0]:
+            print "{} > {}".format(x, a[i][0])
+            p = p*2+1
+        else:
+            print "{} = {} done".format(x, a[i][0])
+            return a[i][0]
+        
+        m = (1 << s[d][3])-1
+        i = rtl[s[d][2]] + s[d][0] + (p&m)*s[d][1]
+    return y
+             
 def try_it(a, h, i, x):
-    print "i={} h={} x={}".format(i, h, x)
+    #print "i={} h={} x={}".format(i, h, x)
     if h == 0:
         if x < a[i][0]:
             print "{} < {}".format(x, a[i][0])
@@ -96,16 +133,20 @@ def try_it(a, h, i, x):
     # we now have the high-order bits on this path
     r = try_it(a, m, i, x)
 
-    print "i0={}".format(i)
+    #print "i0={}".format(i)
     i += 2**(m+1)-1
-    print "i1={}".format(i)
+    #s = 2**(m+1)-1
+    #print "i1={}".format(i)
 
     h = h-m-1
 
     i += r * (2**(h+1)-1)
-    print "i2={}".format(i)
+    #t = (2**(h+1)-1)
+    #print "i2={}".format(i)
 
-    r= r * 2**(m+1) + try_it(a, h, i, x)
+    #print "+{} *{}".format(s, t)
+
+    r = r * 2**(m+1) + try_it(a, h, i, x)
     return r
      
 
@@ -129,6 +170,11 @@ if __name__ == "__main__":
     a = [(a[i][0], a[i][1]+i, a[i][2]+i) for i in range(len(a))]
     print a
     try_it(a, h, 0, 127)
+    try_it2(a, h, 127)
+    print "===="
+    aa = [None]*(h+1)
+    sequencer(h, aa, 0)
+    print aa
 
     
 

9a50a2ef8527df626302051119f2805d10c81e49
Pat Morin <morin@scs.carleton.ca>
2015-04-27 17:04:42 -0400

    Playing with bit tricks

--- src/main.cpp
+++ src/main.cpp
@@ -56,7 +56,7 @@ int main(int argc, char *argv[]) {
 	int n = (argc > 1) ? atoi(argv[1]) : 52;
 
 	//std::random_device rd;   -- using this causes a memory leak
-        auto seed = 23433; // rd();
+	auto seed = 23433; // used a fixed seed instead of random_device
 	std::mt19937 re;
 	std::uniform_int_distribution<int> ui(0, 2*n+1);
 
@@ -66,51 +66,51 @@ int main(int argc, char *argv[]) {
 	int *a = new int[n];
 	for (int i = 0; i < n; i++)
 		a[i] = 2*(i+1);
-    auto stop =  std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed = stop - start;
-    cout << "done (" << elapsed.count() << "s)" << endl;
+	auto stop =  std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> elapsed = stop - start;
+	cout << "done (" << elapsed.count() << "s)" << endl;
 
 	cout << "Building VEB array...";
 	cout.flush();
 	start = std::chrono::high_resolution_clock::now();
-    veb_array<int,int> va(a, n);
-    stop =  std::chrono::high_resolution_clock::now();
-    elapsed = stop - start;
-    cout << "done (" << elapsed.count() << "s)" << endl;
+	veb_array<int,int> va(a, n);
+	stop =  std::chrono::high_resolution_clock::now();
+	elapsed = stop - start;
+	cout << "done (" << elapsed.count() << "s)" << endl;
 
-    re.seed(seed);
-    int m = 30000000;
-    // m = n > m ? n : m;
-    std::cout << "Performing " << m << " VEB searches...";
-    std::cout.flush();
-    start = std::chrono::high_resolution_clock::now();
-    int sum = 0; 
-    for (int i = 0; i < m; i++) {
-      int x = ui(re); 
-      int j = va.search(x);
-      sum += (j < n) ? va.get_data(j) : -1;
-    }
-    stop = std::chrono::high_resolution_clock::now();
-    elapsed = stop - start;
-    std::cout << "done in " << elapsed.count() << "s (sum = " << sum << ")" << std::endl;
+	re.seed(seed);
+	int m = 30000000;
+	// m = n > m ? n : m;
+	std::cout << "Performing " << m << " VEB searches...";
+	std::cout.flush();
+	start = std::chrono::high_resolution_clock::now();
+	int sum = 0;
+	for (int i = 0; i < m; i++) {
+		int x = ui(re);
+		int j = va.search(x);
+		sum += (j < n) ? va.get_data(j) : -1;
+	}
+	stop = std::chrono::high_resolution_clock::now();
+	elapsed = stop - start;
+	std::cout << "done in " << elapsed.count() << "s (sum = " << sum << ")" << std::endl;
 
-    re.seed(seed);
-    std::cout << "Performing " << m << " binary searches...";
-    std::cout.flush();
-    start = std::chrono::high_resolution_clock::now();
-    sum = 0; 
-    for (int i = 0; i < m; i++) {
-      int x = ui(re); 
-      int j = binary_successor2(x, a, n);
-      sum += j < n ? a[j] : -1;
-    }
-    stop = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed2 = stop - start;
-    std::cout << "done in " << elapsed2.count() << "s (sum = " << sum << ")" << std::endl;
+	re.seed(seed);
+	std::cout << "Performing " << m << " binary searches...";
+	std::cout.flush();
+	start = std::chrono::high_resolution_clock::now();
+	sum = 0;
+	for (int i = 0; i < m; i++) {
+		int x = ui(re);
+		int j = binary_successor2(x, a, n);
+		sum += j < n ? a[j] : -1;
+	}
+	stop = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> elapsed2 = stop - start;
+	std::cout << "done in " << elapsed2.count() << "s (sum = " << sum << ")" << std::endl;
 
-    std::cout << "Slowdown factor = " << elapsed2.count()/elapsed.count() << std::endl;
+	std::cout << "Slowdown factor = " << elapsed2.count()/elapsed.count() << std::endl;
 
-    delete[] a;
+	delete[] a;
 }
 
 
--- src/veb_array.h
+++ src/veb_array.h
@@ -40,76 +40,14 @@ protected:
 		sequencer(h1, s, d+h0+1);
 	}
 
-	I copy_data(T *a0, I *rtl, I i, I path, unsigned d) {
-
-		if (d > h || rtl[d] >= n || i >= n) return i;
-		// cout << "d = " << d << ", rtl[d] = " << rtl[d] << endl;
-
-		// visit left child
-		path *= 2;
-		I mask = (1 << (s[d].h0+1))-1;
-		rtl[d+1] = rtl[d-s[d].h0] + (2<<s[d].h0)-1 + (path&mask)*((2<<s[d].h1)-1);
-		i = copy_data(a0, rtl, i, path, d+1);
-
-		// cout << "===== a[" << rtl[d] << "] <- " << a0[i] << endl;
-		// copy data into current node
-		a[rtl[d]] = a0[i++];
-
-		// visit right child
-		path += 1;
-		rtl[d+1] = rtl[d-s[d].h0] + (2<<s[d].h0)-1 + (path&mask)*((2<<s[d].h1)-1);
-		i = copy_data(a0, rtl, i, path, d+1);
-
-		return i;
-	}
+	I copy_data(T *a0, I *rtl, I i, I path, unsigned d);
 
 public:
-	veb_array(T *a0, I n0) {
-		this->n = n0;
+	veb_array(T *a0, I n0);
+	~veb_array();
+	I search(const T &x);
 
-		int m = 1;
-		for (h = 0; m < n; h++, m += 1<<h);
-
-		this->a = new T[n];
-
-		dumdum q = {h, 0};
-		std::fill_n(s, MAX_H+1, q); // to get SIGFAULTs
-		sequencer(h, s, 0);
-
-		I rtl[MAX_H+1];
-		std::fill_n(rtl, MAX_H+1, 32000); // to get SIGFAULTs
-
-		rtl[0] = 0;
-		copy_data(a0, rtl, 0, 0, 0);
-	}
-
-	~veb_array() {
-		delete[] a;
-	}
-
-	I search(const T &x) {
-		I rtl[MAX_H+1];
-
-		I j = n;
-		I i = 0;
-		I p = 0;
-		for (int d = 0; i < n; d++) {
-			rtl[d] = i;
-			if (x < a[i]) {
-				p <<= 1;
-				j = i;
-			} else if (x > a[i]) {
-				p = (p << 1) + 1;
-			} else {
-				return i;
-			}
-			I m = (1 << (s[d].h0+1))-1;
-			i = rtl[d-s[d].h0] + (2<<s[d].h0)-1 + (p&m)*((2<<s[d].h1)-1);
-		}
-		return j;
-	}
-
-	T get_data(const I &i) {
+	const T& get_data(const I &i) {
 		if (i < 0 || i >= n) {
 			std::ostringstream ss;
 			ss << "index " << i << " is out of bounds ({0,...," << n-1 << "})";
@@ -119,6 +57,78 @@ public:
 
 	}
 };
+
+template<class T, class I>
+I veb_array<T,I>::copy_data(T *a0, I *rtl, I i, I path, unsigned d) {
+
+	if (d > h || rtl[d] >= n || i >= n) return i;
+
+	// visit left child
+	path <<= 1;
+	I mask = (1 << (s[d].h0+1))-1;
+	rtl[d+1] = rtl[d-s[d].h0] + (2<<s[d].h0)-1 + (path&mask)*((2<<s[d].h1)-1);
+	i = copy_data(a0, rtl, i, path, d+1);
+
+	a[rtl[d]] = a0[i++];
+
+	// visit right child
+	path += 1;
+	rtl[d+1] = rtl[d-s[d].h0] + (2<<s[d].h0)-1 + (path&mask)*((2<<s[d].h1)-1);
+	i = copy_data(a0, rtl, i, path, d+1);
+
+	return i;
+}
+
+
+template<class T, class I>
+veb_array<T,I>::veb_array(T *a0, I n0) {
+	this->n = n0;
+
+	int m = 1;
+	for (h = 0; m < n; h++, m += 1<<h);
+	h = 0xffffffff >> __builtin_clz(h); // FIXME: portability
+	cout << "h = " << h << endl;
+
+	this->a = new T[n];
+
+	dumdum q = {h, 0};
+	std::fill_n(s, MAX_H+1, q); // to get SIGFAULTs
+	sequencer(h, s, 0);
+
+	I rtl[MAX_H+1];
+	std::fill_n(rtl, MAX_H+1, 32000); // to get SIGFAULTs
+
+	rtl[0] = 0;
+	copy_data(a0, rtl, 0, 0, 0);
+}
+
+template<class T, class I>
+veb_array<T,I>::~veb_array() {
+	delete[] a;
+}
+
+template<class T, class I>
+I veb_array<T,I>::search(const T &x) {
+	I rtl[MAX_H+1];
+
+	I j = n;
+	I i = 0;
+	I p = 0;
+	for (int d = 0; i < n; d++) {
+		rtl[d] = i;
+		if (x < a[i]) {
+			p <<= 1;
+			j = i;
+		} else if (x > a[i]) {
+			p = (p << 1) + 1;
+		} else {
+			return i;
+		}
+		I m = (1 << (s[d].h0+1))-1;
+		i = rtl[d-s[d].h0] + (2<<s[d].h0)-1 + (p&m)*((2<<s[d].h1)-1);
+	}
+	return j;
+}
 
 
 

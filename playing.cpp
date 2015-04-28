328811e30be666cee9a500b2fa463cb73610353d
Pat Morin <morin@scs.carleton.ca>
2015-04-28 11:29:43 -0400

    Reversed path and parity encoding

--- playing.cpp
+++ playing.cpp
@@ -4,6 +4,7 @@
 #include<iostream>
 #include<bitset>
 #include<strstream>
+#include<algorithm>
 #include<cstdlib>
 
 using namespace std;
@@ -12,7 +13,9 @@ string bin_fmt(int x, int d) {
 	bitset<8> bsx(x);
 	ostrstream os;
 	os << bsx;
-	return std::string(os.str()).substr(8-d,d);
+	string s = std::string(os.str()).substr(8-d,d);
+	reverse(s.begin(), s.end());
+	return s;
 }
 
 int main(int argc, char **argv) {
@@ -23,8 +26,9 @@ int main(int argc, char **argv) {
 	}
 
 	int h = atoi(argv[1]);
-       
-	int q = (8*sizeof(h))-__builtin_clz(h); // number of bits in h
+      
+	int w = 8*sizeof(h); 
+	int q = w-__builtin_clz(h); // number of bits in h
 	cout << "q = " << q << endl;
 
 	// make the initial path
@@ -33,7 +37,7 @@ int main(int argc, char **argv) {
 	int parity = 0;   // the parities of the labels on the r-t-l path
 	int label = h;
 	while (label > 0) { 
-		parity = (parity << 1) | (label & 1);
+		parity |= (label & 1) << depth;
 		label >>= 1;	
 		depth++;
 	}
@@ -44,36 +48,35 @@ int main(int argc, char **argv) {
 
 	for (int i = 0; i <= h; i++)  {
 		cout << "=======" << endl;
-		int t1s = __builtin_ctz(~path);
+		int t1s = __builtin_clz(~(path<<(w-depth)));
 		cout << "trailing ones = " << t1s << endl;
 
 		// walk up the right roof ot the current subtree
 		for (int j = 0; j < t1s; j++) {
-			path >>= 1;
 			depth--;
+			path &= ~(1<<depth);
 			label <<= 1;
-			label += (1<<((~parity)&1));
-			parity >>= 1;
+			label += 1<<((~(parity>>depth))&1);
+			parity &= ~(1<<depth);
 		}
 
 		// now step up to the crotch 
-		label <<= 1;
-		label += parity&1;
-		parity >>= 1;
-		path >>= 1;
 		depth--;
+		label <<= 1;
+		label += (parity>>depth)&1;
+		path &= ~(1<<depth);
+		parity &= ~(1<<depth);
 		cout << "crotch label = " << label << endl;
 
 		// step down to right child of crotch
-		path = (path << 1) | 1;
-		depth++;
-		parity = (parity << 1) | (label&1);
+		path |= 1 << depth;
+		parity |= (label&1) << depth;
 		label = (label-1)>>1;
+		depth++;
 
 		// walk down the left roof
 		while (label > 0) { 
-			parity = (parity << 1) | (label & 1);
-			path <<= 1;
+			parity |= (label&1) << depth;
 			label >>= 1;	
 			depth++;
 		}

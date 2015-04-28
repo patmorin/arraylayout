467f31d7db70a59894993aa5ef1e879b63acbc12
Pat Morin <morin@scs.carleton.ca>
2015-04-28 13:05:19 -0400

    Cleaning up redundancies

--- playing.cpp
+++ playing.cpp
@@ -36,11 +36,23 @@ int main(int argc, char **argv) {
 	int depth = 0;    // the depth of the root-to-leaf path
 	int parity = 0;   // the parities of the labels on the r-t-l path
 	int label = h;
+
+	// walk down the left roof from the root
+	/* 
+	 * replacing this code
+     * ----------------snip--------------
 	while (label > 0) { 
 		parity |= (label & 1) << depth;
 		label >>= 1;	
 		depth++;
 	}
+	* ----------------snip--------------
+	* with the following code:
+    */
+	parity = label;
+	depth = w - __builtin_clz(label);
+	label = 0;
+
     cout << "label = " << label << endl;
 	cout << "depth = " << depth << endl;
 	cout << "path = " << bin_fmt(path, depth) << endl;
@@ -51,7 +63,8 @@ int main(int argc, char **argv) {
 		int t1s = __builtin_clz(~(path<<(w-depth)));
 		cout << "trailing ones = " << t1s << endl;
 
-		// walk up the right roof ot the current subtree
+		// walk up the right roof of the current subtree
+		cout << "Walking up right roof of height " << t1s << endl;
 		for (int j = 0; j < t1s; j++) {
 			depth--;
 			path &= ~(1<<depth);
@@ -59,27 +72,41 @@ int main(int argc, char **argv) {
 			label += 1<<((~(parity>>depth))&1);
 			parity &= ~(1<<depth);
 		}
+		cout << "  label = " << label << endl;
 
 		// now step up to the crotch 
-		depth--;
-		label <<= 1;
-		label += (parity>>depth)&1;
-		path &= ~(1<<depth);
-		parity &= ~(1<<depth);
+		// depth--; (C) redundant with D 
+		//           (but use depth -1 in next two formulas)
+		label = (label << 1) | ((parity >> (depth-1)) & 1);
+		// parity &= ~(1<<depth);  // (A) redundant with (B)
+		// path &= ~(1<<depth); // not needed
 		cout << "crotch label = " << label << endl;
 
 		// step down to right child of crotch
-		path |= 1 << depth;
-		parity |= (label&1) << depth;
+		path |= 1 << (depth-1);
+		// parity |= (label&1) << depth; // (B) redundant with (A)
 		label = (label-1)>>1;
-		depth++;
+		// depth++; (D) redundant with (C)
 
 		// walk down the left roof
+		/* Replacing this code
+		 * --------------------snip--------------------
 		while (label > 0) { 
 			parity |= (label&1) << depth;
 			label >>= 1;	
 			depth++;
 		}
+		 * --------------------snip--------------------
+		 * with the following code:
+		 */
+		cout << "label0 = " << label << endl;
+		// FIXME: clz is undefined if label==0, think of something better
+		int down = label == 0 ? 0 : w - __builtin_clz(label);
+		cout << "down = " << down << endl;
+		parity |= label << depth;
+		depth += down;
+		label = 0;
+
 	    cout << "label = " << label << endl;
 		cout << "depth = " << depth << endl;
 		cout << "path = " << bin_fmt(path, depth) << endl;

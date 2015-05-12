9c29b22406f5f5bcfe4af4b82c09c9e711a22cb4
Pat Morin <morin@scs.carleton.ca>
2015-05-11 23:11:45 -0400

    formatting

--- src/btree_array.h
+++ src/btree_array.h
@@ -74,8 +74,8 @@ btree_array<B,T,I,e>::btree_array(ForwardIterator a0, I n0) {
 		throw std::out_of_range(ss.str());
 	}
 	n = n0;
-        int r = posix_memalign((void **)&a, 1UL << 21, sizeof(T) * n);
-        assert(r == 0);
+	int r = posix_memalign((void **)&a, 1UL << 21, sizeof(T) * n);
+	assert(r == 0);
 	copy_data(a0, 0);
 }
 
@@ -87,13 +87,13 @@ btree_array<B,T,I,e>::~btree_array() {
 template<unsigned int B, typename T>
 static const T *inner_search(const T *base, const T x)
 {
-        if (B <= 1) {
-                return base;
-        }
+	if (B <= 1) {
+		return base;
+	}
 
-        const unsigned int half = B / 2;
-        const T *current = &base[half];
-        return inner_search<B - half, T>((*current < x) ? current : base, x);
+	const unsigned int half = B / 2;
+	const T *current = &base[half];
+	return inner_search<B - half, T>((*current < x) ? current : base, x);
 }
 
 template<unsigned B, typename T, typename I, bool early_termination>
@@ -102,38 +102,38 @@ I btree_array<B, T, I, early_termination>::search(const T x) {
 	I j = n;
 	I i = 0;
 	while (i < n) {
-                /* Last (partial block). */
-                if (__builtin_expect(i + B >= n, 0)) {
-                        const T *base = &a[i];
-                        I m = n - i;
+		/* Last (partial block). */
+		if (__builtin_expect(i + B >= n, 0)) {
+			const T *base = &a[i];
+			I m = n - i;
 
-                        while (m > 1) {
-                                I half = m / 2;
-                                const T *current = &base[half];
+			while (m > 1) {
+				I half = m / 2;
+				const T *current = &base[half];
 
-                                base = (*current < x) ? current : base;
-                                m -= half;
-                        }
+				base = (*current < x) ? current : base;
+				m -= half;
+			}
 
-                        I ret = (*base < x) + base - a;
-                        return (ret == n) ? j : ret;
-                }
+			I ret = (*base < x) + base - a;
+			return (ret == n) ? j : ret;
+		}
 
-                const T *base = &a[i];
-                const T *pred = inner_search<B, T>(base, x);
-                unsigned int nth = (*pred < x) + pred - base;
+		const T *base = &a[i];
+		const T *pred = inner_search<B, T>(base, x);
+		unsigned int nth = (*pred < x) + pred - base;
 
-                {
-                        /* nth == B iff x > all values in block. */
-                        const T current = base[nth % B];
-                        I next = i + nth;
+		{
+			/* nth == B iff x > all values in block. */
+			const T current = base[nth % B];
+			I next = i + nth;
 
-                        if (early_termination && current == x) {
-                                return next;
-                        }
+			if (early_termination && current == x) {
+				return next;
+			}
 
-                        j = (current >= x) ? next : j;
-                }
+			j = (current >= x) ? next : j;
+		}
 
 		i = child(nth, i);
 	}

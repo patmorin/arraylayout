3f38a8a039a38bff336b590b8d74f5026629d1fa
Pat Morin <morin@scs.carleton.ca>
2015-05-20 07:09:39 -0400

    Plots are starting to look good

--- scripts/snarf.py
+++ scripts/snarf.py
@@ -2,33 +2,19 @@ import os;
 from collections import defaultdict
 import matplotlib.pyplot as plt
 
-colors = [
-#581B04
-#32E526
-#CF5BFE
-#2D9FCB
-#E9D794
-#F2397E
-#E1530C
-#3B7FF8
-#1C3355
-#20BBA1
-#76850B
-#772E90
-#1D311E
-#430329
-#9C6111
-#AB1B2E
-#C5F0C2
-#F77AA1
-#5F98EE
-#615D05
+# Some maximally dissimilar colors from iwanthue: http://goo.gl/abF97 .
+colours = ['#1FAC7E', '#6D5AD8', '#FD692A', '#F9498D', '#965153',
+           '#FA796D', '#AD1E57']
+
+styles = ['-', '--', ':']
+
+markers = [ "o", "v", "s", "d", "p", "*" ]
 
 
 # Have this return true if you want alg to appear in the plot
 def i_care(alg):
-    return alg.startswith('sorted') or alg.startswith('eytzinger') \
-           or alg.startswith('btree16')    
+    return alg.startswith('eytzinger') or alg.startswith('sorted') \
+           or alg.startswith('btree16')  or alg.startswith('veb')  
     
 if __name__ == "__main__":
     # Snarf the data into a giant nested dictionary.
@@ -52,7 +38,7 @@ if __name__ == "__main__":
         d[alg][dt][it][int(n)] = (float(btime), float(stime), cs)	
 
     # Get the list of algorithms we care about.
-    algs = [alg for alg in stuff[0] if i_care(alg)]
+    algs = sorted([alg for alg in stuff[0] if i_care(alg)])
     data = defaultdict(list)
 
     # Get the list of values of n that are used.
@@ -61,18 +47,37 @@ if __name__ == "__main__":
     # Collect the (n, searchtime) pairs for each algorithm.
     dtype = 'uint32'
     itype = 'uint64'
+    best = defaultdict(lambda: ('none', 10.**6))
+    mx = 0
     for alg in algs:
         for n in ns:
             if n in d[alg][dtype][itype]:
-                data[alg].append((n, d[alg][dtype][itype][n][1]))
+                search_time = d[alg][dtype][itype][n][1]
+                mx = max(mx, search_time)
+                data[alg].append((n, search_time))
+                if search_time < best[n][1]:
+                    best[n] = (alg, search_time)
 
     # Plot everything.
     plt.ioff()
     plt.xscale('log')
-    for alg in sorted(algs):
+    col = 0
+    for alg in algs:
         plt.plot([d[0] for d in data[alg]],
                  [d[1] for d in data[alg]],
-                 label=alg, linewidth=2.0)
+                 label = alg, linewidth = 1.5, 
+                 color = colours[col % len(colours)], 
+                 linestyle = styles[col % len(styles)],
+                 marker = markers[col % len(markers)])
+        col += 1
+
+    # Plot of the winners
+    for n in ns:
+       if best[n][0] in algs:
+           idx = algs.index(best[n][0])
+           plt.plot([n], [mx+.5], color = colours[idx % len(colours)],
+                    marker = markers[idx % len(markers)])
+
     plt.legend()
     plt.show()
 

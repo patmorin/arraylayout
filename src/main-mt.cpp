c21b79e000e7948f6e34780f8fff47ee8ca59d12
Pat Morin <morin@scs.carleton.ca>
2015-07-21 14:45:20 -0400

    Added multithreaded version

--- src/main-mt.cpp
+++ src/main-mt.cpp
@@ -9,6 +9,8 @@
 #include <random>
 #include <cstdint>
 #include <cstring>
+#include <vector>
+#include <thread>
 
 #include "veb_array.h"
 #include "eytzinger_array.h"
@@ -137,15 +139,26 @@ T *build_and_fill(I n) {
 	return a;
 }
 
+std::uint64_t dummy;
 
-// Run tests that perform m searches on one array of size n
-// Note: D is one of uniform_int_distribution or uniform_real_distribution
 template<typename Array, typename T, typename I, typename D>
-void run_test1_b(T *a, I n, I m, const std::string &name) {
-        auto seed=232342;
+void kicker(Array& aa, int k, I n, I m) {
+	auto seed=232342+k;
+    // std::cout << "+";
 	std::mt19937 re(seed);
-	//std::minstd_rand re(seed);
-	D ui(0, 2*n+2);
+	D ui(0, 2*n+1);
+	T sum = 0;
+	for (I i = 0; i < m; i++) {
+		T x = ui(re);
+		I j = aa.search(x);
+		sum += (j < n) ? (int)aa.get_data(j) : -1;
+	}
+	dummy += sum;
+}
+
+template<typename Array, typename T, typename I, typename D>
+void run_test1_b(T *a, I n, I m, I nthreads, const std::string &name) {
+	std::vector<std::thread> threads;
 
 	std::cout << name << " " << type_name<T>() << " " << type_name<I>()
 			<< " " << n << " " << m << " ";
@@ -157,16 +170,14 @@ void run_test1_b(T *a, I n, I m, const std::string &name) {
 	std::cout << elapsed.count() << " ";
 	std::cout.flush();
 	start = std::chrono::high_resolution_clock::now();
-	T sum = 0;
-	for (I i = 0; i < m; i++) {
-		T x = ui(re);
-		I j = aa.search(x);
-		sum += (j < n) ? (int)aa.get_data(j) : -1;
-	}
+	for (I k = 0; k < nthreads; k++)
+		threads.push_back(std::thread(kicker<Array,T,I,D>, std::ref(aa), k, n, m));
+	for (auto &th : threads) th.join();
 	stop = std::chrono::high_resolution_clock::now();
 	elapsed = stop - start;
-	std::cout << elapsed.count() << " " << sum << std::endl;
+	std::cout << elapsed.count() << " mt" << std::endl;
 }
+
 
 // Next we use some traits magic to generate the right kind of random numbers,
 // either integer or "real" depending on T.
@@ -175,36 +186,36 @@ class Tool { };
 
 template<typename Array, typename I>
 struct Tool<Array, std::uint32_t, I> {
-	static void run_test1(std::uint32_t *a, I n, I m, const std::string &name) {
-		run_test1_b<Array, std::uint32_t, I, std::uniform_int_distribution<std::uint32_t> >(a, n, m, name);
+	static void run_test1(std::uint32_t *a, I n, I m, I nthreads, const std::string &name) {
+		run_test1_b<Array, std::uint32_t, I, std::uniform_int_distribution<std::uint32_t> >(a, n, m, nthreads, name);
 	}
 };
 
 template<typename Array, typename I>
 struct Tool<Array, std::uint64_t, I> {
-	static void run_test1(std::uint64_t *a, I n, I m, const std::string &name) {
-		run_test1_b<Array, std::uint64_t, I, std::uniform_int_distribution<std::uint64_t> >(a, n, m, name);
+	static void run_test1(std::uint64_t *a, I n, I m, I nthreads, const std::string &name) {
+		run_test1_b<Array, std::uint64_t, I, std::uniform_int_distribution<std::uint64_t> >(a, n, m, nthreads, name);
 	}
 };
 
 template<typename Array, typename I>
 struct Tool<Array, float, I> {
-	static void run_test1(float *a, I n, I m, const std::string &name) {
-		run_test1_b<Array, float, I, std::uniform_real_distribution<float> >(a, n, m, name);
+	static void run_test1(float *a, I n, I m, I nthreads, const std::string &name) {
+		run_test1_b<Array, float, I, std::uniform_real_distribution<float> >(a, n, m, nthreads, name);
 	}
 };
 
 template<typename Array, typename I>
 struct Tool<Array, double, I> {
-	static void run_test1(double *a, I n, I m, const std::string &name) {
-		run_test1_b<Array, double, I, std::uniform_real_distribution<double> >(a, n, m, name);
+	static void run_test1(double *a, I n, I m, I nthreads, const std::string &name) {
+		run_test1_b<Array, double, I, std::uniform_real_distribution<double> >(a, n, m, nthreads, name);
 	}
 };
 
 template<typename Array, unsigned S, typename I>
 struct Tool<Array, fake_number<std::uint32_t,S>, I> {
-	static void run_test1(fake_number<std::uint32_t,S> *a, I n, I m, const std::string &name) {
-		run_test1_b<Array, fake_number<std::uint32_t,S>, I, std::uniform_int_distribution<std::uint32_t> >(a, n, m, name);
+	static void run_test1(fake_number<std::uint32_t,S> *a, I n, I m, I nthreads, const std::string &name) {
+		run_test1_b<Array, fake_number<std::uint32_t,S>, I, std::uniform_int_distribution<std::uint32_t> >(a, n, m, nthreads, name);
 	}
 };
 
@@ -265,49 +276,42 @@ static const unsigned CACHE_LINE_WIDTH = 64;
 
 // Our test driver
 template<typename T, typename I>
-void run_tests(I n, I m) {
+void run_tests(I n, I m, I nthreads) {
 	T *a = build_and_fill<T,I>(n);
-	Tool<fake_array<T,I>,T,I>::run_test1(a, n, m, "fake");
+	Tool<fake_array<T,I>,T,I>::run_test1(a, n, m, nthreads, "fake");
 
-	Tool<sorted_array<T,I>,T,I>::run_test1(a, n, m, "sorted");
-	Tool<sorted_array_bf<T,I>,T,I>::run_test1(a, n, m, "sorted_bf");
-	Tool<sorted_array_bfp<T,I>,T,I>::run_test1(a, n, m, "sorted_bfp");
-	Tool<sorted_array_stl<T,I>,T,I>::run_test1(a, n, m, "sorted_stl");
+	Tool<sorted_array<T,I>,T,I>::run_test1(a, n, m, nthreads, "sorted");
+	Tool<sorted_array_bf<T,I>,T,I>::run_test1(a, n, m, nthreads, "sorted_bf");
+	Tool<sorted_array_bfp<T,I>,T,I>::run_test1(a, n, m, nthreads, "sorted_bfp");
 
-	Tool<veb_array<T,I>,T,I>::run_test1(a, n, m, "veb");
-	Tool<veb2_array<T,I>,T,I>::run_test1(a, n, m, "veb2");
-//	Tool<veb2_array<T,I,true>,T,I>::run_test1(a, n, m, "veb2e");
+	Tool<veb_array<T,I>,T,I>::run_test1(a, n, m, nthreads, "veb");
 
-	Tool<eytzinger_array<T,I>,T,I>::run_test1(a, n, m, "eytzinger_branchy");
-	Tool<eytzinger_array_bf<T,I>,T,I>::run_test1(a, n, m, "eytzinger_bf");
-	Tool<eytzinger_array_bfp<T,I>,T,I>::run_test1(a, n, m, "eytzinger_bfp");
-	Tool<eytzinger_array_bf<T,I,true>,T,I>::run_test1(a, n, m, "eytzinger_bf_a");
-	Tool<eytzinger_array_bfp<T,I,true>,T,I>::run_test1(a, n, m, "eytzinger_bfp_a");
-//
+	Tool<eytzinger_array<T,I>,T,I>::run_test1(a, n, m, nthreads, "eytzinger_branchy");
+	Tool<eytzinger_array_bf<T,I>,T,I>::run_test1(a, n, m, nthreads, "eytzinger_bf");
+	Tool<eytzinger_array_bfp<T,I>,T,I>::run_test1(a, n, m, nthreads, "eytzinger_bfp");
+	Tool<eytzinger_array_bf<T,I,true>,T,I>::run_test1(a, n, m, nthreads, "eytzinger_bf_a");
+	Tool<eytzinger_array_bfp<T,I,true>,T,I>::run_test1(a, n, m, nthreads, "eytzinger_bfp_a");
+
 	const unsigned B = CACHE_LINE_WIDTH/sizeof(T);
-//	Tool<esmixed_array<T,I,B>,T,I>::run_test1(a, n, m, "esmixed");
-
-//	if (sizeof(I) > 4 || n <= 100000000)
-//		Tool<btree_array<2*B,T,I>,T,I>::run_test1(a, n, m, "btree32");
-//	Tool<btree_array_naive<B,T,I>,T,I>::run_test1(a, n, m, "btree16_naive");
-//	Tool<btree_array<B,T,I>,T,I>::run_test1(a, n, m, "btree16");
-//	Tool<btree_array<B/2,T,I>,T,I>::run_test1(a, n, m, "btree4");
-//
-//	Tool<btree_array_bf<B,T,I>,T,I>::run_test1(a, n, m, "btree16_bf");
-//	Tool<btree_array_bfp<B,T,I>,T,I>::run_test1(a, n, m, "btree16_bfp");
-//	Tool<btree_array_bfp<B/2,T,I>,T,I>::run_test1(a, n, m, "btree8_bfp");
-//	Tool<btree_array_bfp<B/4,T,I>,T,I>::run_test1(a, n, m, "btree4_bfp");
-//
 	if (sizeof(I) > 4 || n <= 100000000)
-		Tool<btree_array<2*B,T,I,true>,T,I>::run_test1(a, n, m, "btree32_a");
-	Tool<btree_array_naive<B,T,I,true>,T,I>::run_test1(a, n, m, "btree16_naive_a");
-	Tool<btree_array<B,T,I,true>,T,I>::run_test1(a, n, m, "btree16_a");
-	Tool<btree_array<B/2,T,I,true>,T,I>::run_test1(a, n, m, "btree8_a");
-//
-	Tool<btree_array_bf<B,T,I,true>,T,I>::run_test1(a, n, m, "btree16_bf_a");
-//	Tool<btree_array_bfp<B,T,I,true>,T,I>::run_test1(a, n, m, "btree16_bfp_a");
-//	Tool<btree_array_bfp<B/2,T,I,true>,T,I>::run_test1(a, n, m, "btree8_bfp_a");
-//	Tool<btree_array_bfp<B/4,T,I,true>,T,I>::run_test1(a, n, m, "btree4_bfp_a");
+		Tool<btree_array<2*B,T,I>,T,I>::run_test1(a, n, m, nthreads, "btree32");
+	Tool<btree_array<B,T,I>,T,I>::run_test1(a, n, m, nthreads, "btree16");
+	Tool<btree_array<B/2,T,I>,T,I>::run_test1(a, n, m, nthreads, "btree4");
+
+	Tool<btree_array_bf<B,T,I>,T,I>::run_test1(a, n, m, nthreads, "btree16_bf");
+	Tool<btree_array_bfp<B,T,I>,T,I>::run_test1(a, n, m, nthreads, "btree16_bfp");
+	Tool<btree_array_bfp<B/2,T,I>,T,I>::run_test1(a, n, m, nthreads, "btree8_bfp");
+	Tool<btree_array_bfp<B/4,T,I>,T,I>::run_test1(a, n, m, nthreads, "btree4_bfp");
+
+	if (sizeof(I) > 4 || n <= 100000000)
+		Tool<btree_array<2*B,T,I,true>,T,I>::run_test1(a, n, m, nthreads, "btree32_a");
+	Tool<btree_array<B,T,I,true>,T,I>::run_test1(a, n, m, nthreads, "btree16_a");
+	Tool<btree_array<B/2,T,I,true>,T,I>::run_test1(a, n, m, nthreads, "btree8_a");
+
+	Tool<btree_array_bf<B,T,I,true>,T,I>::run_test1(a, n, m, nthreads, "btree16_bf_a");
+	Tool<btree_array_bfp<B,T,I,true>,T,I>::run_test1(a, n, m, nthreads, "btree16_bfp_a");
+	Tool<btree_array_bfp<B/2,T,I,true>,T,I>::run_test1(a, n, m, nthreads, "btree8_bfp_a");
+	Tool<btree_array_bfp<B/4,T,I,true>,T,I>::run_test1(a, n, m, nthreads, "btree4_bfp_a");
 
 	delete[] a;
 }
@@ -321,7 +325,7 @@ typedef std::uint_fast32_t index_t;
 
 
 void usage(char *name) {
-	std::cerr << "Usage: " << name << " <dtype> <itype> <n> <m>"
+	std::cerr << "Usage: " << name << " <dtype> <itype> <n> <m> [<nthreads>]"
 			<< std::endl
 			<< "  where " << std::endl
 			<< "  <dtype> is in {uint32, uint64, float32, float64, int128}"
@@ -330,7 +334,7 @@ void usage(char *name) {
 }
 
 template<typename T, typename I>
-void run_it(std::uint64_t n, std::uint64_t m) {
+void run_it(std::uint64_t n, std::uint64_t m, std::uint64_t nthreads) {
 	if (n > std::numeric_limits<I>::max()) {
 		std::ostringstream ss;
 		ss << "array length " << n << " is too big, use a larger I class";
@@ -341,16 +345,16 @@ void run_it(std::uint64_t n, std::uint64_t m) {
 		ss << "number of searches " << m << " is too big, use a larger I class";
 		throw std::out_of_range(ss.str());
 	}
-	run_tests<T,I>((I)n, (I)m);
+	run_tests<T,I>((I)n, (I)m, (I)nthreads);
 }
 
 int main(int argc, char *argv[]) {
-	std::uint64_t n, m;
-	int dt, it;
+	std::uint64_t n, m, nthreads;
+	int dt = 0, it = 1;
 	std::string dtypes[] = {"uint32", "float32", "uint64", "float64", "int128"};
 	std::string itypes[] = {"uint32", "uint64", "fast_uint32"};
 
-	if (argc == 5) {
+	if (argc >= 5) {
 		for (dt = 0; dt < 5; dt++)
 			if (dtypes[dt] == argv[1]) break;
 		for (it = 0; it < 3; it++)
@@ -359,7 +363,12 @@ int main(int argc, char *argv[]) {
 		is_n >> n;
 		std::istringstream is_m(argv[4]);
 		is_m >> m;
-	} else {
+  	    if (argc == 6) {
+  	    	std::istringstream is_nthreads(argv[5]);
+  	    	is_nthreads >> nthreads;
+  	    }
+	}
+	if (argc != 5 && argc != 6) {
 		usage(argv[0]);
 		std::exit(-1);
 	}
@@ -368,40 +377,42 @@ int main(int argc, char *argv[]) {
 		std::exit(-1);
 	}
 
+    std::cout << "n = " << n << ", m = " << m << ", nthreads = " 
+              << nthreads << std::endl;
 	if (dt == 0 && it == 0)
-		run_it<std::uint32_t,std::uint32_t>(n, m);
+		run_it<std::uint32_t,std::uint32_t>(n, m, nthreads);
 	else if (dt == 0 && it == 1)
-		run_it<std::uint32_t,std::uint64_t>(n, m);
+		run_it<std::uint32_t,std::uint64_t>(n, m, nthreads);
 	else if (dt == 0 && it == 2)
-		run_it<std::uint32_t,std::uint_fast32_t>(n, m);
+		run_it<std::uint32_t,std::uint_fast32_t>(n, m, nthreads);
 
 	else if (dt == 1 && it == 0)
-		run_it<float,std::uint32_t>(n, m);
+		run_it<float,std::uint32_t>(n, m, nthreads);
 	else if (dt == 1 && it == 1)
-		run_it<float,std::uint64_t>(n, m);
+		run_it<float,std::uint64_t>(n, m, nthreads);
 	else if (dt == 1 && it == 2)
-		run_it<float,std::uint_fast32_t>(n, m);
+		run_it<float,std::uint_fast32_t>(n, m, nthreads);
 
 	else if (dt == 2 && it == 0)
-		run_it<std::uint64_t,std::uint32_t>(n, m);
+		run_it<std::uint64_t,std::uint32_t>(n, m, nthreads);
 	else if (dt == 2 && it == 1)
-		run_it<std::uint64_t,std::uint64_t>(n, m);
+		run_it<std::uint64_t,std::uint64_t>(n, m, nthreads);
 	else if (dt == 2 && it == 2)
-		run_it<std::uint64_t,std::uint_fast32_t>(n, m);
+		run_it<std::uint64_t,std::uint_fast32_t>(n, m, nthreads);
 
 	else if (dt == 3 && it == 0)
-		run_it<double,std::uint32_t>(n, m);
+		run_it<double,std::uint32_t>(n, m, nthreads);
 	else if (dt == 3 && it == 1)
-		run_it<double,std::uint64_t>(n, m);
+		run_it<double,std::uint64_t>(n, m, nthreads);
 	else if (dt == 3 && it == 2)
-		run_it<double,std::uint_fast32_t>(n, m);
+		run_it<double,std::uint_fast32_t>(n, m, nthreads);
 
 	else if (dt == 4 && it == 0)
-		run_it<fake_number<std::uint32_t,16>,std::uint32_t>(n, m);
+		run_it<fake_number<std::uint32_t,16>,std::uint32_t>(n, m, nthreads);
 	else if (dt == 4 && it == 1)
-		run_it<fake_number<std::uint32_t,16>,std::uint64_t>(n, m);
+		run_it<fake_number<std::uint32_t,16>,std::uint64_t>(n, m, nthreads);
 	else if (dt == 4 && it == 2)
-		run_it<fake_number<std::uint32_t,16>,std::uint_fast32_t>(n, m);
+		run_it<fake_number<std::uint32_t,16>,std::uint_fast32_t>(n, m, nthreads);
 
 	return 0;
 }

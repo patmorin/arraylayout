f72f53047712567f8439b9a7f2dbfcc822691390
Pat Morin <morin@scs.carleton.ca>
2016-08-24 14:02:38 -0400

    Code refactoring and cleanup

--- scripts/experiments-rcr.py
+++ scripts/experiments-rcr.py
@@ -1,3 +1,4 @@
+"""This code is intended to be called by the ../rcr script"""
 from __future__ import division
 import os
 import sys
@@ -34,50 +35,65 @@ def get_free_ram():
 def b2gb(n):
     return n/10**9
 
-if __name__ == "__main__":
-    ram = get_free_ram()
-    N = min(ram//3, 5*2**30)
-    msg = "Detected {:.2g}GB of free RAM, using {:.2g}GB"
-    print(msg.format(b2gb(ram), b2gb(2*N)))
-    base = 1.258925412
-    itypes = ["uint64"]
-    datadir = "data-rcr"
 
-    cpu = get_cpu_number()
-    print("Pinning to CPU {}".format(cpu))
-
-    dtypes = [("uint32", 4), ("uint64", 8), ("uint128", 16)]
-    datafile = datadir + os.path.sep + "alldata.dat"
-    open(datafile, "w").close()  # clobber the data file
+def run_batch(cpu, dtypes, itypes, nt, datafile, N):  
+    """Run a big batch of experiments"""
     for ((dt, size), it) in itertools.product(dtypes, itypes):
-        n = base;
+        base = 10**(1/10)
+        n = base 
         m = 2000000
         while n*size < N:
-            cmd = 'numactl -C {} ./main {} {} {} {} >> {}'
-            cmd = cmd.format(cpu, dt, it, int(n), m, datafile)
+            if nt == 1:
+                cmd = 'numactl -C {} ./main {} {} {} {} >> {}'
+                cmd = cmd.format(cpu, dt, it, int(n), m, datafile)
+            else:
+                cmd = './main-mt {} {} {} {} {} >> {}'
+                cmd = cmd.format(dt, it, int(n), m, nt, datafile)
             print cmd
             if os.system(cmd) != 0:
-                sys.stderr.write("Error while executing '{}': Aborting!\n")
+                errmsg = "Error while executing '{}': Aborting!\n".format(cmd)
+                sys.stderr.write(errmsg)
                 sys.exit(-1)
             while int(n*base) == int(n):
                 n *= base
             n *= base
 
+
+if __name__ == "__main__":
+    """Run our experiments"""
+
+    # Use two-thirds of available RAM, up to 5GB
+    ram = get_free_ram()
+    N = min(ram//3, 5*2**30)
+    msg = "Detected {:.2g}GB of free RAM, using {:.2g}GB"
+    print(msg.format(b2gb(ram), b2gb(2*N)))
+
+    # For single-thread experiments, pin to a single CPU
+    cpu = get_cpu_number()
+    print("Pinning to CPU {}".format(cpu))
+
+    # Always use 64 bit indices
+    itypes = ["uint64"]
+
+    # Put the data here
+    datadir = "data-rcr"
+
+    # Test with 32-, 64-, and 128-bit data
+    dtypes = [("uint32", 4), ("uint64", 8), ("uint128", 16)]
+
+    # Create and clobber the output file
+    datafile = datadir + os.path.sep + "alldata.dat"
+    open(datafile, "w").close()
+
+    # Now do the real work
+    for ((dt, size), it) in itertools.product(dtypes, itypes):
+        run_batch(cpu, dtypes, itypes, 1, datafile, N)
+
+    # Rerun the 32-bit tests using 2, 4, and 8 threads
     for nt in [2, 4, 8]:
         dtypes = [("uint32", 4)]
         datafile = datadir + os.path.sep + "alldata-mt-{}.dat".format(nt)
         open(datafile, "w").close() # clobber the data file
         for ((dt, size), it) in itertools.product(dtypes, itypes):
-            n = base;
-            m = 2000000
-            while n*size < N:
-                cmd = './main-mt {} {} {} {} {} >> {}'
-                cmd = cmd.format(dt, it, int(n), m, nt, datafile)
-                print cmd
-                if os.system(cmd) != 0:
-                    sys.stderr.write("Error while executing '{}': Aborting!\n")
-                    sys.exit(-1)
-                while int(n*base) == int(n):
-                    n *= base
-                n *= base
+            run_batch(0, dtypes, itypes, nt, datafile, N)
 
